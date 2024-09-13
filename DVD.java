81f92524add9ba386717c95359a88e47f01d70f2
FathierAkbarArfani <fathier_arfani_33rpl@student.smktelkom-mlg.sch.id>
2024-09-13 12:47:11 +0700

    Add files via upload

---
+++ DVD.java
@@ -0,0 +1,44 @@
+public class DVD extends Produk {
+    int length;
+    int rating;
+    String studio;
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getRating() {
+        return this.rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getStudio() {
+        return this.studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public DVD() {
+        super();
+        length = 0;
+        rating = 0;
+        studio = "";
+
+        
+    }
+    public DVD(int number, String name, int quantity, double price, int length, int rating, String studio) {
+        super(number , name ,price,quantity);
+        this.length = length;
+        this.rating = rating;
+        this.studio = studio;
+    }
+}
